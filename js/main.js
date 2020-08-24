c86895d737e0b77551c43fb406a92fb51ec51fcb
wayne0926 <3462757126@qq.com>
2020-08-24 23:45:30 +0800

    Site updated: 2020-08-24 23:45:26

--- js/main.js
+++ js/main.js
@@ -805,3 +805,9 @@ $(function () {
   refreshFn()
   unRefreshFn()
 })
+
+// 隐藏背景
+var full_page = document.getElementsByClassName("full_page");
+if (full_page.length != 0) {
+  full_page[0].style.background = "transparent";
+}
